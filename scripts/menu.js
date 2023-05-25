f71f2e8af2fcf80503ed772860955cdd4367e1bd
Robson Colaço <63608891+Robson-C@users.noreply.github.com>
2023-05-25 20:07:26 -0300

    Parte template finalizada

---
+++ scripts/menu.js
@@ -0,0 +1,24 @@
+window.onload = function () {
+    mudouTamanho()
+} 
+
+function clickMenu(){
+    if (itens.style.display == 'block'){
+        itens.style.display = 'none'
+    } else{
+        itens.style.display = 'block'
+    }
+}
+
+function mudouTamanho(){
+    if (window.innerWidth >= 701){
+        burguer.style.display = 'none'
+        itens.style.display = 'none'
+    }else{
+        burguer.style.display = 'block'
+    }
+}
+
+function escolhi(){
+    itens.style.display = 'none'
+}
